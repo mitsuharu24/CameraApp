8aa565b28d528d7a47a852654be66c182a55fb13
mitsuharu24 <mitsuharu.oura@gmail.com>
2022-03-02 17:07:55 +0900

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by 大浦光晴 on 2022/03/02.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
